96ad0604f5269aa9225846ce847240448dc9474a
Barrett Amsrud <barrettamsrud@gmail.com>
2017-06-02 00:18:07 -0500

    Completed number 3

--- 03-Substring-Counter/substring_counter.rb
+++ 03-Substring-Counter/substring_counter.rb
@@ -1,3 +1,24 @@
-def substring(text, dictionary)
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
 
+def substrings(text, dictionary_array)
+
+  # Initialize hash to keep count of instances of dictionary items
+  instances = Hash.new(0)
+
+  # Split the string into an array of words
+  text = text.split(" ")
+
+  # Check each word with the dictionary
+  text.each do |word|
+    dictionary_array.each do |item|
+      if word.downcase.include? item
+        instances[item] += 1
+      end
+    end
+  end
+  # Return the hash of instances
+  return instances
 end
+
+puts substrings("below", dictionary)
+puts substrings("Howdy partner, sit down!  How's it going?", dictionary)
